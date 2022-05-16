5542a3c7e13e18db35305f57b973d5d61ed46b07
Brian Calhoon <bcalhoon@msi-inc.com>
2022-05-16 09:23:13 -0400

    BC

--- scripts/BC_immunizations.R
+++ scripts/BC_immunizations.R
@@ -314,7 +314,55 @@ ggsave("viz/Immunizations.png",
        height=4,
        width=7)
 
-
+#fully immunized year 2 ----
+ggplot(filter(dat_immun2_ful
+              , subpop == "imm2")
+       , aes(x = mnthyr
+             , y = rate_fix
+             , group = subpop
+             , color = subpop)) +
+  geom_point(alpha = .6, size = 1) + 
+  geom_smooth(size = .7
+              , se = FALSE) +
+#  geom_segment(data = targets
+ #              , aes(x = x1
+  #                   , xend = x2
+   #                  , y = y1
+    #                 , yend = y2)
+     #          , color = "maroon"
+      #         , inherit.aes = FALSE) +
+  #geom_text(data = targets
+   #         , aes(x = x2
+    #              , y = y1
+     #             , label = paste0(y1*100, "%"))
+      #      , color = "maroon"
+       #     , vjust = -.4
+        #    , size = 3
+         #   , inherit.aes = FALSE)+
+  scale_y_continuous(limits = c(0,1),
+                     labels = percent) +
+  labs(title = "Proportion of infants who are fully vaccinated within 2 years, (2018-2022)"
+       #, subtitle = "Immunization rates rise during spring and fall campaigns"
+       , x = ""
+       , y = ""
+       , caption = "Source: Zambia Ministry of Health") +
+  scale_color_manual(name = "",
+                     values = usaid_palette) +
+  theme(plot.title.position = "plot"
+        , plot.title = element_text(size = 14)
+        , axis.title.x = element_text(size = 12)
+        , axis.title.y = element_text(size = 12)
+        , axis.text = element_text(size = 9)
+        , legend.title = element_text(size = 12) 
+        , legend.text = element_text(size = 7)
+        , legend.position = "none"
+  ) 
+#save the viz
+ggsave("viz/fully imm 2 year.png",
+       device="png",
+       type="cairo",
+       height=4,
+       width=7)
 #Same viz with a geom_smooth() and not a geom_line() ----
 ggplot(dat_immun2, aes(x = mnthyr
                        , y = rate_fix
@@ -412,9 +460,9 @@ color = c("#205493"
           , "#A7C6ED")                      
                       
 subtitle <- "Proportion of infants <span style = 'color:#205493;'>breastfed within an hour after birth </span>,<br>
- <span style = 'color:#BA0C2F;'> exclusively breasfed until 6 months</span> and who received <span style = 'color:#A7C6ED;'>Vitamin A at 6 and 11 months</span>"
+ <span style = 'color:#BA0C2F;'> exclusively breastfed until 6 months</span> and who received <span style = 'color:#A7C6ED;'>Vitamin A at 6 and 11 months</span>"
 
-#basic line chart of immunization data  ----
+#basic line chart of child health data  ----
 ggplot(dat_nutri, aes(x = mnthyr
                        , y = rate_fix
                        , group = subpop
@@ -458,7 +506,7 @@ ggplot(dat_nutri, aes(x = mnthyr
                       , group = subpop
                       , color = subpop
                       , label = subpop)) +
-  geom_smooth(method = lm
+  geom_smooth(method = loess
               , size = .7
               , se = FALSE
               , alpha = .6) +
@@ -487,6 +535,49 @@ ggplot(dat_nutri, aes(x = mnthyr
 
 #save the viz
 ggsave("viz/Nutrition_smooth.png",
+       device="png",
+       type="cairo",
+       height=4,
+       width=7)
+
+
+#Breastmilk within 1 hour chart
+#smooth chart of immunization data  ----
+ggplot(filter(dat_nutri, subpop == "breastmilk_1h")
+              , aes(x = mnthyr
+                      , y = rate_fix
+                      , group = subpop
+                      , color = subpop
+                      , label = subpop)) +
+  geom_point(alpha = .6, size = 1)+
+  geom_smooth(method = loess
+              , size = .7
+              , se = FALSE
+              , alpha = .6) +
+  scale_y_continuous(limits = c(0,1),
+                     labels = percent) +
+  labs(title = "Proportion of infants breastfeeding within 1 hour, 2018-2022"
+       , x = ""
+       , y = ""
+       , caption = "Source: Zambia Ministry of Health") +
+  scale_color_manual(name = ""
+                     , labels = c("Infants receiving breastmilk <= 1 hour"
+                                  , "Infants exclusively breastfed at 6 months" 
+                                  , "Vitamin A coverage")
+                     , values = usaid_palette) +
+  theme(plot.title.position = "plot",
+        plot.title = element_text(size = 14, hjust = 0),
+        plot.subtitle = ggtext::element_markdown(),
+        axis.title.x = element_text(size = 12),
+        axis.title.y = element_text(size = 12),
+        axis.text = element_text(size = 9),
+        legend.title = element_text(size = 12), 
+        legend.text = element_text(size = 11)
+        , legend.position = "none"
+  ) 
+
+#save the viz
+ggsave("viz/breastfed within 1 hour.png",
        device="png",
        type="cairo",
        height=4,
