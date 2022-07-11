99ec5eae14a8e8955f17f7a6c6b831dbf3f667e2
Nyimbili <shida.nyimbili@tetratech.com>
2022-07-11 21:53:11 +0200

    ns

--- scripts/r prep.R
+++ scripts/r prep.R
@@ -24,7 +24,8 @@ zam_packages <- c("tidyverse", "here", "gifski", "gt","gtsummary","gghighlight",
                   "psych","labelled","readxl", "skimr","sjlabelled","sjstats","sjPlot", "sjmisc","readr","ggrepel","openxlsx",
                   "sysfonts","scales","magrittr","knitr","lubridate","Hmisc","haven","tidyr","reshape2","DescTools","extrafont",
                   "patchwork", "extrafontdb", "ggtext", "geomtextpath","zoo","psych","cowplot","kableExtra","ztable","pheatmap",
-                  "RColorBrewer", "data.table", "d3heatmap","hablar", "gganimate", "PhantomJS", "dygraphs", "gapminder", "hrbrthemes", "geomtextpath")
+                  "RColorBrewer", "data.table", "d3heatmap","hablar", "gganimate", "PhantomJS", "dygraphs", "gapminder", "hrbrthemes",
+                  "geomtextpath", "patchwork")
 
 
 
@@ -60,7 +61,7 @@ base <- theme_bw() + theme(panel.grid.minor.x=element_blank(),
                            axis.text.x = element_text(angle = 0, family="Gill Sans Mt"),
                            legend.text=element_text(size=10, family="Gill Sans Mt"),
                            # legend.position = "bottom",
-                           legend.position = c(.70,.99),
+                           legend.position = c(.83,.99),
                            legend.justification = c("left", "top"),
                            legend.box.just = "left",
                            strip.text=element_text(size=13, family="Gill Sans Mt"),
