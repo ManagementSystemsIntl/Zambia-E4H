d058c0be8c893c4e1f3921e18ad1ac8ae733bf7b
Nyimbili <shida.nyimbili@tetratech.com>
2022-08-02 17:09:44 +0200

    ns

--- scripts/April-June 2022/E4H Child health indicators (Apr-Jun 2022).R
+++ scripts/April-June 2022/E4H Child health indicators (Apr-Jun 2022).R
@@ -28,7 +28,7 @@ End <- as.Date(NULL)
 chldHq <- read_xls("data/Jan- Jun 2022/Child Health Data_National Level(Quarterly).xls")
 chldHqp <- read_xls("data/Jan-Mar 2022/Child Health Data_Provincial Level(Quarterly).xls")
 chldH_prov <- read_xls("data/Jan- Jun 2022/Child Health Data_Provincial Level(Monthly).xls")
-
+names(chldH_prov)
 chldH_prov
 chldH_prov  <- chldH_prov  %>%
   mutate(month_chr = str_sub(periodname,
@@ -52,22 +52,23 @@ sum(chldH_prov$month_chr!=chldH_prov$month) # expecting 0 if vars same
 
 #'* MEASLES 1 & 2*
 names(chldH)
+# view(chldH)
 chldH <- chldH %>%
-  rename(ancc = 3,
-         anc1 = 4,
-         anc1u20 = 5) %>%
-  mutate(anccp = ancc/100,
-         anc1p = anc1/100,
-         anc1u20p = anc1u20/100)
+  rename(msles1 = 11,
+         msles2 = 12
+         ) %>%
+  
+  mutate(msles1p = msles1/100,
+         msles2p = msles2/100)
 
-#'*set anccp to 100 for all values >100*
+#'*set msles1p & msles2p to 100 for all values >100*
 chldH <- chldH %>% 
-  dplyr::mutate(ancc = ifelse(ancc > 100, 100, ancc)) %>% 
-  dplyr::mutate(anccp = ancc/100)
+  dplyr::mutate(ancc = ifelse(msles1 > 100, 100, msles1)) %>% 
+  dplyr::mutate(msles1p = msles1/100)
 
 #'*To create legend, gather method for including a legend --*
 
-chldH <- gather(chldH, key = subpop , value = rate, c(anccp, anc1p,anc1u20p))
+chldH <- gather(chldH, key = subpop , value = rate, c(msles1p, msles2p))
 chldH$subpop <- factor(chldH$subpop, levels = unique(chldH$subpop)) # transform into factor
 levels(chldH$subpop)
 
@@ -75,24 +76,262 @@ levels(chldH$subpop)
 
 ggplot(chldH, aes(x = mnthyr, y = rate, group = subpop, colour = subpop)) +
   geom_point(alpha=.6, size=1.9) + 
-  geom_line(size=1) +
-  #geom_smooth(method = loess, size = .8, se=FALSE) +
+  #geom_line(size=1) +
+  geom_smooth(method = loess, size = .8, se=FALSE) +
   scale_y_continuous(limits = c(0,1),
                      labels = percent,
                      breaks = c(.1,.2,.3,.4,.5,.6,.7,.8,.9, 1)) +
   scale_x_date(date_labels="%b %Y",date_breaks="4 months") +
-  xlab("") + 
-  ylab("") +
-  ggtitle("Proportion of expected pregnancies receiving antenatal care (ANC), 2018-2022") +
+  labs(x="", y="", caption="Data Source: HMIS", title="Proportion of under 1year infants who received the vaccine has been declining since June 2021,  \nwhilest overall under 2 years trend has been steadly increasing") +
   scale_color_manual(name ="",
                      values = usaid_palette,
-                     labels = c("1st ANC coverage (all trimesters)", "1st ANC Coverage (1st Trimester)", 
-                                "1st ANC visits in the 1st trimester: Women <20 yrs")
+                     labels = c("Measle under 1", "Measeles under 2")
   ) + 
   base
 
-ggsave("viz/Apr-Jun 2022/Reproductive chldHernal & Neontal/Proportion of expected pregnancies receiving antenatal care geom-line ns1.png",
+ggsave("viz/Apr-Jun 2022/Child Health/Proportion of infacts receiving Measles Vaccines.png",
        device="png",
        type="cairo",
-       height = 5.5,
-       width = 10)
+       height = 6.5,
+       width = 12)
+
+
+#'* MEASLES 1 & 2 SEPERATED BY SUPPORTED AND NON USAID PROVINCES* ----
+names(chldH_prov)
+
+msle_prov <- chldH_prov %>%
+  rename(prov = 1,
+         msles1 = 11,
+         msles2 = 12
+  ) %>%
+  
+  mutate(msles1p = msles1/100,
+         msles2p = msles2/100) %>%
+
+
+  select(prov, mnthyr, msles1p, msles2p) %>% 
+  mutate(prov = factor(prov),
+         ip = case_when(prov=="Northern" |
+                          prov =="Central" |
+                          prov =="Luapula" |
+                          prov =="Muchinga" |
+                          prov =="Southern" |
+                          prov =="Eastern" ~ "ip",
+                        TRUE ~ "non-ip"))
+  
+  table(msle_prov$ip, msle_prov$prov)
+  frq(msle_prov$ip) #sjmisc
+  
+  levels(msle_prov$prov)
+  
+  #'*redraw for all USAID-funded provinces*
+  
+  ipfunded <- msle_prov %>%
+    filter(ip =="ip")
+  
+  levels(msle_prov$prov)
+  
+  ipfunded1 <- ipfunded %>% 
+    gather(key = subpop , value = rate, c(msles1p,msles2p)) %>% 
+    mutate(ip = factor(ip),
+           subpop = factor(subpop))
+  
+  levels(ipfunded1$ip)
+  levels(ipfunded1$subpop)
+  
+  ip_fundedprov <- ggplot(ipfunded1, aes(x = mnthyr, y = rate, group = subpop, colour = subpop)) +
+    geom_point(alpha=.6, size=.6) + 
+    # geom_area(alpha=.3, size=.8, color=usaid_red, fill=usaid_red) + 
+    #geom_line(size=1) +
+    geom_smooth(method = loess, size = .8, se=FALSE)  +
+    facet_wrap(~prov) +
+    faceted +
+    scale_y_continuous(limits = c(0,1),
+                       labels = percent,
+                       breaks = c(.2,.4,.6,.8, 1)) +
+    labs(x ="", y="", caption = "") +
+    ggtitle("USAID-supported provinces") +
+    scale_color_manual(name= "", values = (usaid_palette)) + baseX
+  
+  ip_fundedprov
+  
+  
+  #'*redraw for all Non USAID-funded provinces*
+  
+  ipNot_funded <- msle_prov %>%
+    filter(ip =="non-ip")
+  
+  levels(msle_prov$prov)
+  
+  ipNot_funded1 <- ipNot_funded %>% 
+    gather(key = subpop , value = rate, c(msles1p,msles2p)) %>% 
+    mutate(ip = factor(ip),
+           subpop = factor(subpop))
+  
+  levels(ipNot_funded1$ip)
+  levels(ipNot_funded1$subpop)
+  
+  ipNot_fundedprov <- ggplot(ipNot_funded1, aes(x = mnthyr, y = rate, group = subpop, colour = subpop)) +
+    #geom_area(alpha=.3, size=.8,color=usaid_blue, fill=light_blue) + 
+    geom_point(alpha=.6, size=.6) + 
+    #geom_line(size=1) +
+    geom_smooth(method = loess, size = .8, se=FALSE)  +
+    facet_wrap(~prov) +
+    faceted +
+    scale_y_continuous(limits = c(0,1),
+                       labels = percent,
+                       breaks = c(.2,.4,.6,.8, 1)) +
+    labs(x ="", y="", caption = "Data Source: HMIS") +
+    ggtitle("USAID-supported provinces") +
+    scale_color_manual(name= "", values = (usaid_palette), labels = c("Measle under 1", "Measeles under 2")) + basey
+  
+  ipNot_fundedprov
+  
+  ip_fundedprov + ipNot_fundedprov + plot_layout(guides = "collect")
+  
+  ggsave("viz/Apr-Jun 2022/Child Health/Province Proportion of infacts receiving Measles Vaccines faceted ns.png",
+         device="png",
+         type="cairo",
+         height = 6.5,
+         width = 12)
+  
+  
+  
+  #'* BCG Immunization*
+  names(chldH)
+  # view(chldH)
+  bcgImz <- chldH %>%
+    rename(bcg = 10) %>%
+    
+    mutate(bcgp = bcg/100)
+  
+  #'*set msles1p & msles2p to 100 for all values >100*
+  bcgImz1 <- bcgImz %>% 
+    dplyr::mutate(bcg = ifelse(bcg > 100, 100, bcg)) %>% 
+    dplyr::mutate(bcgp = bcg/100)
+  
+  #'*To create legend, gather method for including a legend --*
+  
+  bcgImz1 <- gather(bcgImz1, key = subpop , value = rate, c(bcg))
+  bcgImz1$subpop <- factor(bcgImz1$subpop, levels = unique(bcgImz1$subpop)) # transform into factor
+  levels(bcgImz1$subpop)
+  
+  # view(chldH)
+  
+  ggplot(bcgImz1, aes(x = mnthyr, y = bcgp, colour=usaid_blue )) +
+    geom_point(alpha=.4, size=1.9) + 
+    #geom_line(size=1) +
+    geom_smooth(method = loess, size = .8, se=FALSE) +
+    scale_y_continuous(limits = c(0,1),
+                       labels = percent,
+                       breaks = c(.1,.2,.3,.4,.5,.6,.7,.8,.9, 1)) +
+    scale_x_date(date_labels="%b %Y",date_breaks="4 months") +
+    labs(x="", y="", caption="Data Source: HMIS", title="Proportion of infants who received BCG under 1 has been on a decline trend, with slightly steady periods around June 2021 , \nthis decline trend started June 2019") +
+    scale_color_manual(name ="",
+                       values = usaid_blue,
+                       labels = "BCG Under 1") + baseX
+  
+  ggsave("viz/Apr-Jun 2022/Child Health/BCG Vaccines.png",
+         device="png",
+         type="cairo",
+         height = 6.5,
+         width = 12)
+  
+  
+  #'* BCG SEPERATED BY SUPPORTED AND NON USAID PROVINCES* ----
+  names(chldH_prov)
+  
+  bcg_Prov <- chldH_prov %>%
+    rename(prov =1,
+           bcg = 10) %>%
+    
+    mutate(bcgP = bcg/100) %>%
+    
+    
+    select(prov, mnthyr, bcgP) %>% 
+    mutate(prov = factor(prov),
+           ip = case_when(prov=="Northern" |
+                            prov =="Central" |
+                            prov =="Luapula" |
+                            prov =="Muchinga" |
+                            prov =="Southern" |
+                            prov =="Eastern" ~ "ip",
+                          TRUE ~ "non-ip"))
+  
+  table(bcg_Prov$ip, bcg_Prov$prov)
+  frq(bcg_Prov$ip) #sjmisc
+  
+  levels(bcg_Prov$prov)
+  
+  #'*redraw for all USAID-funded provinces*
+  
+  ipfunded <- bcg_Prov %>%
+    filter(ip =="ip")
+  
+  levels(bcg_Prov$prov)
+  
+  ipfunded1 <- ipfunded %>% 
+    gather(key = subpop , value = rate, c(bcgP)) %>% 
+    mutate(ip = factor(ip),
+           subpop = factor(subpop))
+  
+  levels(ipfunded1$ip)
+  levels(ipfunded1$subpop)
+  
+  ip_fundedprov <- ggplot(ipfunded1, aes(x = mnthyr, y = rate, group = subpop, colour = subpop)) +
+    geom_point(alpha=.6, size=.6) + 
+    #geom_area(alpha=.3, size=.8, color=usaid_blue, fill=light_blue) + 
+    #geom_line(size=1) +
+    geom_smooth(method = loess, size = .8, se=FALSE)  +
+    facet_wrap(~prov) +
+    faceted +
+    scale_y_continuous(limits = c(0,1),
+                       labels = percent,
+                       breaks = c(.2,.4,.6,.8, 1)) +
+    labs(x ="", y="", caption = "") +
+    ggtitle("USAID-supported provinces") +
+    scale_color_manual(name= "", values = usaid_blue) + baseX
+  
+  ip_fundedprov
+  
+  
+  #'*redraw for all Non USAID-funded provinces*
+  
+  ipNot_funded <- bcg_Prov %>%
+    filter(ip =="non-ip")
+  
+  levels(bcg_Prov$prov)
+  
+  ipNot_funded1 <- ipNot_funded %>% 
+    gather(key = subpop , value = rate, c(bcgP)) %>% 
+    mutate(ip = factor(ip),
+           subpop = factor(subpop))
+  
+  levels(ipNot_funded1$ip)
+  levels(ipNot_funded1$subpop)
+  
+  ipNot_fundedprov <- ggplot(ipNot_funded1, aes(x = mnthyr, y = rate, group = subpop, colour = subpop)) +
+    #geom_area(alpha=.3, size=.8,color=usaid_red, fill=usaid_red) + 
+    geom_point(alpha=.6, size=.6) + 
+    #geom_line(size=1) +
+    geom_smooth(method = loess, size = .8, se=FALSE)  +
+    facet_wrap(~prov) +
+    faceted +
+    scale_y_continuous(limits = c(0,1),
+                       labels = percent,
+                       breaks = c(.2,.4,.6,.8, 1)) +
+    labs(x ="", y="", caption = "Data Source: HMIS") +
+    ggtitle("USAID-supported provinces") +
+    scale_color_manual(name= "", values = usaid_red, labels = c("BCG Under 1")) + baseX
+  
+  ipNot_fundedprov
+  
+  ip_fundedprov + ipNot_fundedprov + plot_layout(guides = "collect")
+  
+  ggsave("viz/Apr-Jun 2022/Child Health/Province BCG Vaccines faceted smooth ns.png",
+         device="png",
+         type="cairo",
+         height = 7.5,
+         width = 12)
+  
+  
