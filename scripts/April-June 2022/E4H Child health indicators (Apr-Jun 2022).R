8cb65d9eb35de3825bf0110a1cde9dfb5c95722f
Nyimbili <shida.nyimbili@tetratech.com>
2022-08-02 15:01:32 +0200

    ns

--- scripts/April-June 2022/E4H Child health indicators (Apr-Jun 2022).R
+++ scripts/April-June 2022/E4H Child health indicators (Apr-Jun 2022).R
@@ -2,3 +2,97 @@
 # Child health indicators
 # April - June 2022
 
+source("scripts/r prep2.r")
+
+chldH <- read_xls("data/Jan- Jun 2022/Child Health Data_National Level(Monthly).xls")
+chldH  <- chldH  %>%
+  mutate(month_chr = str_sub(periodname,
+                             start=1,
+                             end=nchar(periodname)-5),
+         month = factor(month_chr,
+                        levels=c("January","February","March","April","May","June","July","August","September","October","November","December")),
+         month_code = as.numeric(month), 
+         year = str_sub(periodname, 
+                        start=nchar(periodname)-4,
+                        end=nchar(periodname)),
+         monyr = paste(month_code, year, sep="-"),
+         mnthyr = my(monyr))
+
+sum(chldH$month_chr!=chldH$month) # expecting 0 if vars same
+
+pnctrgts <- read_xls("data/Jan- Jun 2022/PNC Targets.xls")
+Start <- as.Date(NULL)
+End <- as.Date(NULL)
+
+
+chldHq <- read_xls("data/Jan- Jun 2022/Child Health Data_National Level(Quarterly).xls")
+chldHqp <- read_xls("data/Jan-Mar 2022/Child Health Data_Provincial Level(Quarterly).xls")
+chldH_prov <- read_xls("data/Jan- Jun 2022/Child Health Data_Provincial Level(Monthly).xls")
+
+chldH_prov
+chldH_prov  <- chldH_prov  %>%
+  mutate(month_chr = str_sub(periodname,
+                             start=1,
+                             end=nchar(periodname)-5),
+         month = factor(month_chr,
+                        levels=c("January","February","March","April","May","June","July","August","September","October","November","December")),
+         month_code = as.numeric(month), 
+         year = str_sub(periodname, 
+                        start=nchar(periodname)-4,
+                        end=nchar(periodname)),
+         monyr = paste(month_code, year, sep="-"),
+         mnthyr = my(monyr))
+
+sum(chldH_prov$month_chr!=chldH_prov$month) # expecting 0 if vars same
+
+
+
+
+#'*IMMUNIZATION*
+
+#'* MEASLES 1 & 2*
+names(chldH)
+chldH <- chldH %>%
+  rename(ancc = 3,
+         anc1 = 4,
+         anc1u20 = 5) %>%
+  mutate(anccp = ancc/100,
+         anc1p = anc1/100,
+         anc1u20p = anc1u20/100)
+
+#'*set anccp to 100 for all values >100*
+chldH <- chldH %>% 
+  dplyr::mutate(ancc = ifelse(ancc > 100, 100, ancc)) %>% 
+  dplyr::mutate(anccp = ancc/100)
+
+#'*To create legend, gather method for including a legend --*
+
+chldH <- gather(chldH, key = subpop , value = rate, c(anccp, anc1p,anc1u20p))
+chldH$subpop <- factor(chldH$subpop, levels = unique(chldH$subpop)) # transform into factor
+levels(chldH$subpop)
+
+# view(chldH)
+
+ggplot(chldH, aes(x = mnthyr, y = rate, group = subpop, colour = subpop)) +
+  geom_point(alpha=.6, size=1.9) + 
+  geom_line(size=1) +
+  #geom_smooth(method = loess, size = .8, se=FALSE) +
+  scale_y_continuous(limits = c(0,1),
+                     labels = percent,
+                     breaks = c(.1,.2,.3,.4,.5,.6,.7,.8,.9, 1)) +
+  scale_x_date(date_labels="%b %Y",date_breaks="4 months") +
+  xlab("") + 
+  ylab("") +
+  ggtitle("Proportion of expected pregnancies receiving antenatal care (ANC), 2018-2022") +
+  scale_color_manual(name ="",
+                     values = usaid_palette,
+                     labels = c("1st ANC coverage (all trimesters)", "1st ANC Coverage (1st Trimester)", 
+                                "1st ANC visits in the 1st trimester: Women <20 yrs")
+  ) + 
+  base
+
+ggsave("viz/Apr-Jun 2022/Reproductive chldHernal & Neontal/Proportion of expected pregnancies receiving antenatal care geom-line ns1.png",
+       device="png",
+       type="cairo",
+       height = 5.5,
+       width = 10)
