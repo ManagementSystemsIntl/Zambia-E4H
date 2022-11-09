f0c2bd1fd13c359499a98069b4241e7f8db570fa
Shida Nyimbili <Shida.Nyimbili@righttocare-zambia.org>
2022-11-09 21:55:10 +0200

    ns

---
+++ scripts/prematurity.r
@@ -0,0 +1,18 @@
+
+source("scripts/r prep2.r")
+
+mat <- read_xls("data/Jan- Jun 2022/Reproductive Maternal Data_National Level(Monthly).xls")
+mat  <- mat  %>%
+  mutate(month_chr = str_sub(periodname,
+                             start=1,
+                             end=nchar(periodname)-5),
+         month = factor(month_chr,
+                        levels=c("January","February","March","April","May","June","July","August","September","October","November","December")),
+         month_code = as.numeric(month), 
+         year = str_sub(periodname, 
+                        start=nchar(periodname)-4,
+                        end=nchar(periodname)),
+         monyr = paste(month_code, year, sep="-"),
+         mnthyr = my(monyr))
+
+sum(mat$month_chr!=mat$month) # expecting 0 if vars same
