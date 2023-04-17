4c43349a8da5fad25188ff01498371113b54dbb1
Sawulu <pimpa.sawulu@tetratech.com>
2023-04-18 00:00:25 +0200

    ps

--- scripts/prematurity.r
+++ scripts/prematurity.r
@@ -624,7 +624,7 @@ peri.dth.prv  <- peri.dth.prv  %>%
                              start=1,
                              end=nchar(periodname)-5),
          month = factor(month_chr,
-                        levels=c("January","February","March")),
+                        levels=c("January","February","March","April","May","June","July","August","September","October","November","December")),
          month_code = as.numeric(month), 
          year = str_sub(periodname, 
                         start=nchar(periodname)-4,
