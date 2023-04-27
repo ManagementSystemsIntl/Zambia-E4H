132b6bbeb14b0d3c543ac975cf976ad4b4868feb
Sawulu <pimpa.sawulu@tetratech.com>
2023-04-27 11:08:34 +0200

    ps

--- scripts/prematurity.r
+++ scripts/prematurity.r
@@ -438,6 +438,102 @@ ggsave("viz/prematurity Apr 2023/q1 perinatal_mortality_rate.png",
        height = 6.5,
        width = 15)
 
+#'*REDRAW FOR MATERNAL MORTALITY RATE*
+
+maternal.mort <- read_xls("data/prematurity April 2023/Maternal Mortality Ratio 2019_2023 yearly.xls")
+maternal.mort  <- maternal.mort  %>%
+  mutate(year = str_sub(period,
+                        start=nchar(period)-4,
+                        end=nchar(period)))
+maternal.mort
+
+maternal.mort1 <- maternal.mort %>%
+  select(1,2,4) %>%
+  na.omit()
+
+maternal.mort2 <- maternal.mort1 %>%
+  rename(prov =1,
+         mat.mr=2,
+         yr=3)
+
+maternal.mort2
+
+maternal.mort3 <- maternal.mort2 %>% 
+  gather(key = subRt , value = rate, c(peri.mr))
+
+maternal.mort3
+
+zam.boundary <- geoboundaries(country = "Zambia"
+                              , adm_lvl = 1) %>% 
+  select(shapeName)
+
+zam.boundary
+
+zam.boundary1 <- zam.boundary %>%
+  select(1, 2) %>%
+  na.omit()
+
+zam.boundary1
+
+
+map_colors <- carto_pal(name = "Burg")
+
+
+maternal.mort4 <- maternal.mort3 %>%
+  group_by(yr,prov, subRt)
+
+
+maternal.mort4
+
+maternal.mort5 <- left_join(maternal.mort4
+                             , zam.boundary1
+                             , by = c("prov" = "shapeName")) %>%
+  sf::st_as_sf()
+
+maternal.mort5
+
+
+ggplot(maternal.mort5, aes(geometry = geometry, fill = rate)) +
+  geom_sf()+
+  geom_sf_text(aes(label = prov), size = 3) +
+  facet_wrap(~yr) +
+  scale_fill_carto_c(name="Proportion of\n Mortality Rate"
+                     , palette = "Burg") +
+  labs(x="", y="", caption = "Data Source: HMIS",
+       title = "Maternal Mortality Rates, 2019 - 2023"
+       , subtitle = "Darker colors represent a higher proportion of mortality rate") + #for faceted and xy labels include x="Longitude", y="Latitude", +faceted
+  theme_void() +
+  theme(plot.title.position = "plot",
+        plot.title = element_text(size = 16, hjust=0.5, family="Gill Sans Mt", face="bold"),
+        plot.subtitle = element_text(size = 12, hjust=0.5),
+        plot.caption = element_text(size=11),
+        # axis.title.x = element_text(size = 12, family="Gill Sans Mt", face="bold"),
+        # axis.title.y = element_text(size = 12, family="Gill Sans Mt", face="bold"),
+        # axis.text.x = element_text(size = 8),
+        # axis.text.y = element_text(size = 10),
+        legend.text = element_text(size = 12),
+        legend.title=element_blank(),
+        legend.position="right",
+        strip.text=element_text(size=14, family="Gill Sans Mt"))
+
+##interactive map  
+# zam.boundary2 %>%
+#   leaflet() %>%
+#   addTiles() %>%
+#   addPolygons(label = zam.boundary2$shapeName)
+
+ggsave("viz/prematurity Apr 2023/Maternal_mortality_rate_map.png",
+       device="png",
+       type="cairo",
+       height = 6.5,
+       width = 15)
+
+
+
+
+
+
+
 #'*Prematurity rate*
 prema.rate <- read_xlsx("data/prematurity April 2023/prematurity rate.xlsx")
 
