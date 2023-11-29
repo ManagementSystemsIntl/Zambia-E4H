a85959fcf508074e2c4ae44768ce220183aeee17
Sawulu <pimpa.sawulu@tetratech.com>
2023-11-29 05:54:24 +0200

    ps

--- scripts/FP Demo_ps.R
+++ scripts/FP Demo_ps.R
@@ -20,47 +20,9 @@ ggplot(FP_Data_Provincial_3, aes(Year, Value, fill = Method)) +
 #This is to try the bars
 source("scripts/r prep2.r")
 source("scripts/r prep3.r")
-
-fpd <- read_xlsx("data/Aug 2023 MHDR/FP Type disaggregations_provincial.xlsx")
-
-fpd
-fpd1 <- fpd %>% 
-  rename(prov=1,
-         yr=2)
-
-fpd1
-
-fpd1_plt <- ggplot(fpd1, aes(x=yr, y=Value, fill="Method"), alpha=0.6)+ 
-  geom_bar(alpha=.7, stat="identity", position="dodge") +
-  facet_wrap(~prov, ncol=4) +
-  faceted +
-  scale_fill_manual(values=c( usaid_palette6)) +
-  scale_y_continuous(labels=comma) +
-  labs(caption = "Data Source: HMIS",
-       x="",
-       y="Number of distributed Contraceptives", fill = "") +
-  #theme(legend.position = "bottom") +
-  ggtitle("Family Planning Methods and their consumption levels, 2021-2023 Q2.") +
-  scale_color_manual(name ="",
-                     values = usaid_palette,
-                     labels = c("CopperT", "Hormonal US", "Implanon", "Jadelle",
-                                "Medroxyprogesterone injection DMPA-IM", "Medroxyprogesterone injection DMPA-SC")
-  ) + 
-  base
-
-fpd1_plt
-ggsave("viz/Dec 23 FHDR/number of distributed FP by province.png",
-       device="png",
-       type="cairo",
-       height = 6.0,
-       width = 11)
-
-
-
-
 library(scales)
 
-fpd <- read_xlsx("data/Aug 2023 MHDR/FP Type disaggregations_provincial.xlsx")
+fpd <- read_xls("data/Dec 2023 MHDR/FP Type disaggregations_provincial.xls")
 
 fpd
 fpd1 <- fpd %>% 
@@ -73,17 +35,17 @@ options(scipen = 999)
 ggplot(fpd1, aes(yr, Value, fill = Method)) +
   geom_bar(stat= "identity") +
   facet_wrap(~prov) +
-  ggtitle("Family Planning Methods and their consumption levels, 2021-2023 Q2.") +
+  ggtitle("Family Planning Methods and their consumption levels, Jan 2021 - Sept 2023.") +
   scale_y_continuous(labels = scales::label_number_si()) +
   scale_fill_manual(values=c( usaid_palette6)) +
   labs(x = "", y = "Number of distributed contraceptives", fill = "Method/Type", caption = "Data Source: HMIS") +
   theme(legend.position = "bottom")
 
-ggsave("viz/Aug 23 FHDR/number of distributed FP by province.png",
+ggsave("viz/Dec 23 FHDR/number of distributed FP by province.png",
        device="png",
        type="cairo",
        height = 6.0,
-       width = 11)
+       width = 12)
 
 
 
