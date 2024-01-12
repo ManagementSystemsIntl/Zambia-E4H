6afe311cb13ba8b63668948849f597cbeadb8258
Sawulu <pimpa.sawulu@tetratech.com>
2024-01-12 15:42:32 +0200

    ps

--- scripts/prematurity.r
+++ scripts/prematurity.r
@@ -401,6 +401,89 @@ ggsave("viz/Prematurity viz jan 24/perinatal_mortality_rate.png",
        height = 6.5,
        width = 12.5)
 
+
+#'*Redraw for Quarter 4s.................*
+
+provinces_zam <- st_read("Data/Updated Shapefiles/Updated_Province.shp")
+perinatal.mort <- read_xlsx("Data/Prematurity Jan 2024/perinatal mortality rate q4s_2019_2023.xlsx")
+perinatal.mort
+perinatal.mort  <- perinatal.mort  %>%
+  mutate(year = str_sub(period,
+                        start=nchar(period)-4,
+                        end=nchar(period)))
+perinatal.mort
+perinatal.mort1 <- perinatal.mort %>%
+  select(1,2,4) %>%
+  na.omit()
+perinatal.mort2 <- perinatal.mort1 %>%
+  rename(prov =1,
+         peri.mr=2,
+         yr=3)
+perinatal.mort2
+perinatal.mort3 <- perinatal.mort2 %>%
+  gather(key = subRt , value = rate, c(peri.mr))
+perinatal.mort3
+#zam.boundary <- geoboundaries(country = "Zambia"
+#, adm_lvl = 1) %>%
+#select(shapeName)
+#zam.boundary
+#write_xlsx(zam.boundary,"data/prematurity/province.xlsx")
+provinces_zam1 <- provinces_zam %>%
+  select(1, 2) %>%
+  na.omit()
+provinces_zam1
+
+map_colors <- carto_pal(name = "Burg")
+
+perinatal.mort4 <- perinatal.mort3 %>%
+  group_by(yr,prov, subRt)
+
+perinatal.mort4
+perinatal.mort5 <- left_join(perinatal.mort4
+                             , provinces_zam1
+                             , by = c("prov" = "PROVINCE")) %>%
+  sf::st_as_sf()
+perinatal.mort5
+
+ggplot(perinatal.mort5, aes(geometry = geometry, fill = rate)) +
+  geom_sf()+
+  geom_sf_text(aes(label = prov), size = 3) +
+  facet_wrap(~yr) +
+  scale_fill_carto_c(name="Proportion of\n Mortality Rate"
+                     , palette = "Blue") +
+  labs(x="", y="", caption = "Data Source: MPDSR & HMIS",
+       title = "Perinatal Mortality Rate, Quarters 4 (2019 - 2023)"
+       , subtitle = "Darker colors represent a higher proportional rate") + #for faceted and xy labels include x="Longitude", y="http://127.0.0.1:28939/graphics/plot_zoom_png?width=923&height=900Latitude", +faceted
+  theme_void() +
+  theme(plot.title.position = "plot",
+        plot.title = element_text(size = 16, hjust=0.5, family="Gill Sans Mt", face="bold"),
+        plot.subtitle = element_text(size = 12, hjust=0.5),
+        plot.caption = element_text(size=11),
+        # axis.title.x = element_text(size = 12, family="Gill Sans Mt", face="bold"),
+        # axis.title.y = element_text(size = 12, family="Gill Sans Mt", face="bold"),
+        # axis.text.x = element_text(size = 8),
+        # axis.text.y = element_text(size = 10),
+        legend.text = element_text(size = 12),
+        legend.title=element_blank(),
+        legend.position="right",
+        strip.text=element_text(size=14, family="Gill Sans Mt"))
+
+
+ggsave("viz/Prematurity viz jan 24/perinatal_mortality_rate qrts 4.png",
+       device="png",
+       type="cairo",
+       height = 6.5,
+       width = 12.5)
+
+
+
+
+
+
+
+
+
+
 #'*REDRAW FOR MATERNAL MORTALITY RATE*
 
 maternal.mort <- read_xls("data/prematurity April 2023/Maternal Mortality Ratio 2019_2023 yearly.xls")
