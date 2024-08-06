106878427d6c709d6f1590b685302d8b83370778
Sawulu <pimpa.sawulu@tetratech.com>
2024-08-06 17:10:03 +0200

    PS

--- scripts/prematurity.r
+++ scripts/prematurity.r
@@ -1011,14 +1011,14 @@ nat_pmr <- ggplot(peri.mr2, aes(x=mnthyr, y=perinatal.mortRate, colour=usaid_red
   geom_smooth(method = loess, linewidth = .8, se=FALSE) +
   scale_y_continuous(labels=comma) +
   scale_x_date(date_labels="%b %y",date_breaks="3 months") +
-  labs(x="", y="", caption="Data Source: PDSR & HMIS", title="Perinatal Mortality Rate per 1,000 live births, had been on a downward trend from January 2022, \nbut has begun to rise begining Jan 2023 and remained almost constant upto 2024.") +
+  labs(x="", y="", caption="Data Source: PDSR & HMIS", title="Perinatal Mortality Rate per 1,000 live births, had been on a downward trend from January 2020, \nbut has begun to rise begining Jan 2023.") +
   scale_color_manual(name ="",
                      values = usaid_red,
                      labels ="Perinatal Mortality Rates") + 
   basey
 
 nat_pmr
-ggsave("viz/Prematurity viz jan 24/National perinatal mortality rates 2020-2023.png",
+ggsave("viz/Prematurity viz jan 24/National perinatal mortality rates Jan 2020-2023.png",
        device="png",
        type="cairo",
        height = 6.5,
